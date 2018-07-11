5fe6a1928794238c7c8e99e913e7aaee21c4e6b2
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-07-11 23:43:16 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -6,40 +6,41 @@ ingredients = [
     ["mayo", "mustard", "tabasco"]
 ]
 
-def sandwich_request(ingredients, order)
-  order.each do |want|
-    found = ingredients.find do |arr|
-      arr.find {|have| want == have}
-    end
-    return false if !found
-  end
-  true
-end
+ingredients 
+# def sandwich_request(ingredients, order)
+#   order.each do |want|
+#     found = ingredients.find do |arr|
+#       arr.find {|have| want == have}
+#     end
+#     return false if !found
+#   end
+#   true
+# end
 
-# example city_info hash
-cities = {
-  :new_york => {
-    :mayor => "Bill DeBlasio",
-    :population => 8337000,
-    :website => "http://www.nyc.gov"
-  },
-  :los_angeles => {
-    :mayor => "Eric Garcetti",
-    :population => 3884307,
-    :website => "http://www.lacity.org"
-  },
-  :miami => {
-    :mayor => "Tomás Regalado",
-    :population => 419777,
-    :website => "http://www.miamigov.com"
-  },
-  :chicago => {
-    :mayor => "Rahm Emanuel",
-    :population => 2695598,
-    :website => "http://www.cityofchicago.org/"
-  }
-}
+# # example city_info hash
+# cities = {
+#   :new_york => {
+#     :mayor => "Bill DeBlasio",
+#     :population => 8337000,
+#     :website => "http://www.nyc.gov"
+#   },
+#   :los_angeles => {
+#     :mayor => "Eric Garcetti",
+#     :population => 3884307,
+#     :website => "http://www.lacity.org"
+#   },
+#   :miami => {
+#     :mayor => "Tomás Regalado",
+#     :population => 419777,
+#     :website => "http://www.miamigov.com"
+#   },
+#   :chicago => {
+#     :mayor => "Rahm Emanuel",
+#     :population => 2695598,
+#     :website => "http://www.cityofchicago.org/"
+#   }
+# }
 
-def city_populations(cities)
-  cities.values.sum {|city| city[:population]}
-end
+# def city_populations(cities)
+#   cities.values.sum {|city| city[:population]}
+# end
